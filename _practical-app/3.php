3031f21ef5c1dc3729cad3f67ef2482d8d98abac
Leonardo <leonardo38361@gmail.com>
2024-07-26 02:37:08 -0300

    exercise 3

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -1,20 +1,20 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
+		<?php Navigation(); ?>
+
 	</aside><!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+	<article class="main-content col-xs-8">
 
-<?php  
+		<?php
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+		/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
 
 
@@ -25,14 +25,54 @@
 
  */
 
-	
-?>
+		$voteForPHP = 10;
+		if ($voteForPHP <= 6) {
+			echo "I don't like PHP";
+		} elseif ($voteForPHP >= 7 && $voteForPHP <= 9) {
+			echo "I like PHP";
+		} else {
+			echo "I love PHP";
+		}
+
+		echo '<br>';
+		for ($i = 0; $i <= 10; $i++) {
+			if ($i != 10) {
+				echo $i . ', ';
+			} else {
+				echo $i;
+			}
+		}
+
+		echo '<br>';
+		$number = 6;
+
+		switch ($number) {
+			case 1:
+				echo "The number is 1";
+				break;
+			case 2:
+				echo "The number is 2";
+				break;
+			case 3:
+				echo "The number is 3";
+				break;
+			case 4:
+				echo "The number is 4";
+				break;
+			case 5:
+				echo "The number is 5";
+				break;
+			default:
+				echo "The number can not be greater than 5";
+		}
+
+		?>
 
 
 
 
 
 
-</article><!--MAIN CONTENT-->
-	
-<?php include "includes/footer.php"; ?>
+	</article><!--MAIN CONTENT-->
+
+	<?php include "includes/footer.php"; ?>
