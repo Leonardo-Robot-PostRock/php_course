fff419cc786c472cf24e3c9ed553fbefc92d0931
Leonardo <leonardo38361@gmail.com>
2024-07-25 21:56:28 -0300

    arrays

---
+++ section2/array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+
+<body>
+    <?php
+    $numberList = array(23, 64, 88, 100, 1002, 321, 5639, '21', '66', '<h1>Hello world</h1>');
+    $numberList2 = [];
+
+    print_r($numberList);
+    echo '<br>';
+    echo $numberList[0];
+    ?>
+</body>
+
+</html>
