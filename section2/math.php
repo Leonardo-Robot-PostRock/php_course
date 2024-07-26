dfd2525b9a6e038f96f833f83eee8c3da0e46009
Leonardo <leonardo38361@gmail.com>
2024-07-25 21:44:57 -0300

    math

---
+++ section2/math.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math</title>
+</head>
+
+<body>
+    <?php
+    echo 56 + 47;
+    echo "<br>";
+    echo 50 - 20;
+    echo "<br>";
+    echo 46 * 19;
+    echo "<br>";
+    echo 56 / 12;
+    echo "<br>";
+    echo "<br>";
+    echo 45 + 35 * 21 / 214 - 39;
+    echo "<br>";
+    echo (5 + 5) * 10;
+    echo "<br>";
+    echo 5 + 5 * 10;
+    echo "<br>";
+    $number1 = 12;
+    $number2 = 24;
+
+    echo $number1 * $number2;
+    ?>
+</body>
+
+</html>
