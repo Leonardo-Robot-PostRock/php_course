2cd099d7b97f5f3870a951ecb941f7be8b14c737
Leonardo <leonardo38361@gmail.com>
2024-07-26 02:14:55 -0300

    for loop

---
+++ section3/for_loop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Loop</title>
+</head>
+
+<body>
+    <?php
+    for ($counter = 0; $counter < 10; $counter++) {
+        echo $counter . ' ';
+    }
+    ?>
+</body>
+
+</html>
