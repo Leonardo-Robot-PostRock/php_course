4bc5896ce629a6c26bbea7508f9a678acca03f6c
Leonardo <leonardo38361@gmail.com>
2024-07-27 01:49:54 -0300

    exercise 4

--- _practical-app/4.php
+++ _practical-app/4.php
@@ -1,36 +1,54 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-		<?php Navigation();?>
-			
-		
+		<?php Navigation(); ?>
+
+
 	</aside><!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+	<article class="main-content col-xs-8">
 
-	
-	<?php  
 
-/*  Step1: Define a function and make it return a calculation of 2 numbers
+		<?php
+
+		/*  Step1: Define a function and make it return a calculation of 2 numbers
 
 	Step 2: Make a function that passes parameters and call it using parameter values
 
 
  */
 
-	
-?>
+		function calculate(){
+			$number1 = 20;
+			$number2 = 40;
+
+			return $number1 * $number2;
+		}
+
+		echo calculate() . '<br>';
+
+		function dynamicCalculation($number1, $number2)
+		{
+			$result = $number1 * $number2;
+			return $result;
+		}
+
+		$result = dynamicCalculation(2, 4);
+
+		echo $result;
+
+		?>
 
 
 
 
 
-</article><!--MAIN CONTENT-->
+	</article><!--MAIN CONTENT-->
 
 
-<?php include "includes/footer.php"; ?>
+	<?php include "includes/footer.php"; ?>
