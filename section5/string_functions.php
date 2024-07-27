e7c306bfeb71ad8de3329eb1748a935a57640757
Leonardo <leonardo38361@gmail.com>
2024-07-27 02:30:24 -0300

    strings functions

---
+++ section5/string_functions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $strings = "Hello students do you like the class";
+
+    echo strlen($strings);
+    echo '<br>';
+    echo strtoupper($strings);
+    echo '<br>';
+    echo strtolower($strings);
+    ?>
+</body>
+
+</html>
