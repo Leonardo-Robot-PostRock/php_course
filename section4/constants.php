563d7bc181da58dec6551c947875c8b829d12d02
Leonardo <leonardo38361@gmail.com>
2024-07-27 00:38:52 -0300

    more examples

--- section4/constants.php
+++ section4/constants.php
@@ -17,8 +17,51 @@
     //constant variable
     define("NAME", 'Leonardo');
     // other way
-    const CONSTANT = 'Hello!!!';
-    echo CONSTANT . ' ' . NAME;
+    const CONSTANT_DEMO = 'Hello!!!';
+    echo CONSTANT_DEMO . ' ' . NAME;
+
+    const ANIMALS_DEMO = ['dog', 'cat', 'bird'];
+
+    echo '<br>';
+
+    foreach(ANIMALS_DEMO as $animal){
+        echo $animal . PHP_EOL;
+    }
+
+    // Works as of PHP 5.3.0
+
+    const CONSTANT = 'Hello World';
+
+
+    echo CONSTANT;
+
+
+    // Works as of PHP 5.6.0
+
+    const ANOTHER_CONST = CONSTANT . '; Goodbye World';
+
+    echo ANOTHER_CONST;
+
+
+    const ANIMALS_V1 = array('dog', 'cat', 'bird');
+
+    echo ANIMALS_V1[1]; // outputs "cat"
+
+
+    // Works as of PHP 7
+
+    define('ANIMALS', array(
+
+        'dog',
+
+        'cat',
+
+        'bird'
+
+    ));
+
+    echo ANIMALS[1]; // outputs "cat"
+
     ?>
 </body>
 
