a5defbdd062d0fff6fd5f4562b6fc25abc5462b5
Leonardo <leonardo38361@gmail.com>
2024-07-27 02:32:33 -0300

    comments

--- section5/string_functions.php
+++ section5/string_functions.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strings functions</title>
 </head>
 
 <body>
     <?php
+    // https://www.php.net/manual/en/ref.strings.php
     $strings = "Hello students do you like the class";
 
     echo strlen($strings);
