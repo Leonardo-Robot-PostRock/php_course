46316ad81ce648ec4bbf872d5c1acf686261c54d
Leonardo <leonardo38361@gmail.com>
2024-07-27 02:41:18 -0300

    arrays

---
+++ section5/array_functions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $list = [231, 124, 543, 776, 223, 145, 347, 975];
+
+    echo max($list);
+    echo '<br>';
+    echo min($list);
+    echo '<br>';
+    echo sort($list);
+    ?>
+</body>
+
+</html>
