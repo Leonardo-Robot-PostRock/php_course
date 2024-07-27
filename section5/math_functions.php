c7de67819cd5476c104805730e720e7dce9e3b27
Leonardo <leonardo38361@gmail.com>
2024-07-27 02:06:44 -0300

    math functions

---
+++ section5/math_functions.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math functions</title>
+</head>
+
+<body>
+    <?php
+    echo pow(2, 6);
+    echo "<br>";
+    echo rand(1, 100);
+    echo "<br>";
+    echo sqrt(100);
+    echo "<br>";
+    echo ceil(4.5);
+    echo "<br>";
+    echo floor(2.6);
+    echo "<br>";
+    echo round(2.5);
+    
+    ?>
+</body>
+
+</html>
