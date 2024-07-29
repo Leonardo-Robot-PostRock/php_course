a5f31d51410c0e0a257d2d249dc0961e133a2ce2
Leonardo <leonardo38361@gmail.com>
2024-07-29 01:30:25 -0300

    exercise 6

--- _practical-app/6.php
+++ _practical-app/6.php
@@ -1,29 +1,44 @@
-
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<style>
+	input {
+		margin-bottom: 10px;
+	}
+</style>
 
-		<aside class="col-xs-4">
-		
-		<?php Navigation();?>
-			
-		</aside><!--SIDEBAR-->
+<section class="content">
+
+	<aside class="col-xs-4">
+
+		<?php Navigation(); ?>
+
+	</aside><!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
- 
+	<article class="main-content col-xs-8">
 
-	<?php  
 
-/*  Step1: Make a form that submits one value to POST super global
+		<form action="6.php" method="post">
+			<input type="text" name="fname" placeholder="Enter username">
+			<br>
+			<input type="password" placeholder="Enter username">
+			<br>
+			<input type="submit" name="submit">
+		</form>
+		<?php
+
+		/*  Step1: Make a form that submits one value to POST super global
 
 
  */
+		if (isset($_POST['submit'])) {
+			$name = $_POST['fname'];
+			echo "Hello! " . $name;
+		}
 
-	
-?>
+		?>
 
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php"; ?>
+	</article><!--MAIN CONTENT-->
+	<?php include "includes/footer.php"; ?>
