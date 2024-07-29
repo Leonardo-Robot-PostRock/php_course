b6d2b19a902c554e6758db4e44b0d0ab929433bb
Leonardo <leonardo38361@gmail.com>
2024-07-29 01:09:37 -0300

    Validating the Form values

--- section6/form.php
+++ section6/form.php
@@ -1,17 +1,35 @@
 <?php
 
 if (isset($_POST['submit'])) {
+    $name  = array("Edwin", "Student", "Peter", "Leonardo", "Mohad", "Samid", "Jane", "Tom");
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    $minimum = 5;
+    $maximun = 10;
+
     echo "Hello " . $username . '<br>';
-    echo "This is your password" . $password;
+    echo "This is your password " . $password . "<br>";
+
+    if (strlen($username) < $minimum) {
+        echo "Username has to be longer than 5";
+    }
+
+    if (strlen($username) > $maximun) {
+        echo "Username cannot be longer than 10";
+    }
+
+    if (!in_array($username, $name)) {
+        echo "Sorry you are not allowed";
+    }else{
+        echo "Welcome";
+    }
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    echo $username;
-}
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $username = $_POST['username'];
+//     echo $username;
+// }
 
 ?>
 
@@ -27,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <form action="form.php" method="post">
         <input type="text" placeholder="Enter Username" name="username">
-        <input type="password" placeholder="Enter Password">
+        <input type="password" placeholder="Enter Password" name="password">
         <br>
         <input type="submit" name="submit">
     </form>
