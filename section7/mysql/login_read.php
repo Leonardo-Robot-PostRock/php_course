4dc14b0ceb94843258b2d5abf46022f40abef571
Leonardo <leonardo38361@gmail.com>
2024-07-30 03:42:47 -0300

    show the data in a table

--- section7/mysql/login_read.php
+++ section7/mysql/login_read.php
@@ -2,48 +2,61 @@
 
 $connection = mysqli_connect('localhost', 'root', '', 'loginapp', 3307);
 
-if ($connection) {
-    echo "We are connected";
-} else {
-    die("Database connection failed");
+if (!$connection) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
 
-$query = "SELECT * FROM users ";
-
+$query = "SELECT * FROM users";
 $result = mysqli_query($connection, $query);
 
 if (!$result) {
-    die('Query FAILED' . mysqli_error($connection));
+    die('Query FAILED: ' . mysqli_error($connection));
 }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Read</title>
+    <title>Read users table</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="container">
-        <div class="col-6 col-sm-6">
-            <?php
-            while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-                <pre>
+    <div class="container min-vh-100 d-flex justify-content-center align-items-center">
+        <div class="col-12 col-sm-12">
+            <h1 class="text-center">Users table</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Password</th>
+                    </tr>
+                </thead>
+                <tbody>
                     <?php
-                    print_r($row);
+                    while ($row = mysqli_fetch_assoc($result)) {
                     ?>
-                </pre>
-            <?php
-            }
-            ?>
+                        <tr>
+                            <th scope="row"><?php echo htmlspecialchars($row['id']); ?></th>
+                            <td><?php echo htmlspecialchars($row['username']); ?></td>
+                            <td><?php echo htmlspecialchars($row['password']); ?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </body>
 
 </html>
+
+<?php
+mysqli_close($connection);
+?>
