bf4db9264aa0c0bc8b944436c4b160973a39a1ce
Leonardo <leonardo38361@gmail.com>
2024-07-30 00:39:12 -0300

    get users

---
+++ section7/mysql/login_update.php
@@ -0,0 +1,41 @@
+<?php include "db.php";?>
+<?php include "functions.php";?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-xs-6">
+            <form action="login_update.php" method="post">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <select name="id" id="">
+
+                        <?php
+                        showData();
+                        ?>
+                    </select>
+                </div>
+                <input type="submit" name="submit" value="Submit" class="btn btn-primary mt-2">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
